1cf8c92a982fa65e675a3f2c7a2faba9f5a3b626
Srikant Shubham <srikantsubham10000@gmail.com>
2023-03-15 17:45:29 +0530

    zeet hosting changes

--- app.py
+++ app.py
@@ -172,4 +172,4 @@ def submit():
     # return render_template('predict.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=False, use_reloader=True,host='0.0.0.0')
